5eff08b69ca8da48d12542da79f2f539f418e956
yfme01 <admin@yfme.me>
2022-04-25 20:48:52 +0800

    .

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -10,6 +10,7 @@ sed -i "/\/etc\/coremark\.sh/d" `find package/ -follow -type f -path '*/coremark
 sed -i 's/192.168.1.1/192.168.2.1/' package/base-files/files/bin/config_generate
 sed -i 's/=1/=0/g' package/kernel/linux/files/sysctl-br-netfilter.conf
 
+# sed -i '/DEPENDS/ s/$/ +libcap-bin/' `find package/ -follow -type f -path '*/luci-app-openclash/Makefile'`
 sed -i '/DEPENDS+/ s/$/ +wsdd2/' `find package/ -follow -type f -path '*/ksmbd-tools/Makefile'`
 sed -i '/DEPENDS/ s/$/ +frpc/' `find package/ -follow -type f -path '*/luci-app-frpc/Makefile'`
 
@@ -79,14 +80,3 @@ sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon"' 
 
 # remove the mirros from cn
 sed -i '/182.140.223.146/d;/\.cn\//d;/tencent/d' scripts/download.pl
-
-# add r1s support to Lean's repo
-if [[ $DEVICE == 'r1s' ]]; then
-  cd ~ && git clone -b openwrt-21.02 https://github.com/immortalwrt/immortalwrt && cd immortalwrt
-  git log --grep r1s -i | grep '^commit ' | head -n -2 | cut -d' ' -f2 | tac | xargs git show | sed '0,/UENV/s//ATF/' > r1s.diff
-  git show 124116564e8a6081e79cb2e87b0d87b2af99c583 >> r1s.diff
-  cd ~/lede && chmod +x target/linux/sunxi/base-files/etc/board.d/* && git apply ~/immortalwrt/r1s.diff
-  echo "\nCONFIG_ARM_ALLWINNER_SUN50I_CPUFREQ_NVMEM=y\nCONFIG_ARM_SCPI_CPUFREQ=y" >> target/linux/sunxi/cortexa53/config-5.4
-elif [[ $DEVICE == 'r2s' || $DEVICE == 'r2c' || $DEVICE == 'r1p' || $DEVICE == 'r1p-lts' ]]; then
-  sed -i 's/5.15/5.4/g' target/linux/rockchip/Makefile
-fi
