e71c131948eb2bf91338e95278b912cbbec7db99
yfme01 <admin@yfme.me>
2022-07-01 12:47:05 +0800

    .

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -11,7 +11,6 @@ sed -i 's/192.168.1.1/192.168.2.1/' package/base-files/files/bin/config_generate
 sed -i 's/=1/=0/g' package/kernel/linux/files/sysctl-br-netfilter.conf
 
 sed -i '/DEPENDS+/ s/$/ +wsdd2/' `find package/ -follow -type f -path '*/ksmbd-tools/Makefile'`
-sed -i '/DEPENDS/ s/$/ +frpc/' `find package/ -follow -type f -path '*/luci-app-frpc/Makefile'`
 
 sed -i 's/ +ntfs-3g/ +ntfs3-mount/' `find package/ -follow -type f -path '*/automount/Makefile'`
 sed -i '/skip\=/ a skip=`mount | grep -q /dev/$device; echo $?`' `find package/ -follow -type f -path */automount/files/15-automount`
@@ -36,7 +35,7 @@ sed -i $line_number_INCLUDE_V2ray'd' package/custom/openwrt-passwall/luci-app-pa
 sed -i $line_number_INCLUDE_V2ray'd' package/custom/openwrt-passwall/luci-app-passwall/Makefile
 
 
-if [ $BRANCH == 'master' ]; then
+if [[ $BRANCH == 'master' ]]; then
 
   # fix po path for snapshot
   #find package/ -follow -type d -path '*/po/zh-cn' | xargs dirname | xargs -n1 -i sh -c "rm -f {}/zh_Hans; ln -sf zh-cn {}/zh_Hans"
@@ -54,15 +53,23 @@ if [ $BRANCH == 'master' ]; then
   # swap the network adapter driver to r8168 to gain better performance for r4s
   #sed -i 's/r8169/r8168/' target/linux/rockchip/image/armv8.mk
 
-  # change the voltage value for over-clock stablization
-  config_file_cpufreq=`find package/ -follow -type f -path '*/luci-app-cpufreq/root/etc/config/cpufreq'`
-  truncate -s-1 $config_file_cpufreq
-  echo -e "\toption governor0 'schedutil'" >> $config_file_cpufreq
-  echo -e "\toption minfreq0 '816000'" >> $config_file_cpufreq
-  echo -e "\toption maxfreq0 '1512000'\n" >> $config_file_cpufreq
 
-  # fix fan control
-  sed -i 's/pwmchip1/pwmchip0/' target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol.sh target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol-direct.sh
+  case $DEVICE in
+    r2s|r2c|r1p|r1p-lts)
+      # change the voltage value for over-clock stablization
+      config_file_cpufreq=`find package/ -follow -type f -path '*/luci-app-cpufreq/root/etc/config/cpufreq'`
+      truncate -s-1 $config_file_cpufreq
+      echo -e "\toption governor0 'schedutil'" >> $config_file_cpufreq
+      echo -e "\toption minfreq0 '816000'" >> $config_file_cpufreq
+      echo -e "\toption maxfreq0 '1512000'\n" >> $config_file_cpufreq
+
+      # add pwm fan control service
+      wget https://github.com/friendlyarm/friendlywrt/commit/cebdc1f94dcd6363da3a5d7e1e69fd741b8b718e.patch
+      git apply cebdc1f94dcd6363da3a5d7e1e69fd741b8b718e.patch
+      rm cebdc1f94dcd6363da3a5d7e1e69fd741b8b718e.patch
+      sed -i 's/pwmchip1/pwmchip0/' target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol.sh target/linux/rockchip/armv8/base-files/usr/bin/fa-fancontrol-direct.sh
+      ;;
+  esac
 
 fi
 
@@ -89,9 +96,6 @@ done
 # set default theme to argon
 sed -i '/uci commit luci/i\uci set luci.main.mediaurlbase="/luci-static/argon"' `find package -type f -path '*/default-settings/files/*-default-settings'`
 
-# remove the mirros from cn
-sed -i '/182.140.223.146/d;/\.cn\//d;/tencent/d' scripts/download.pl
-
 # add r1s support to Lean's repo
 if [[ $DEVICE == 'r1s' ]]; then
   cd ~ && git clone -b openwrt-21.02 https://github.com/immortalwrt/immortalwrt && cd immortalwrt
@@ -101,13 +105,16 @@ if [[ $DEVICE == 'r1s' ]]; then
   merge_package https://github.com/immortalwrt/immortalwrt/branches/openwrt-18.06-k5.4/package/emortal/autocore
 fi
 
-if [[ $DEVICE == 'r4s' || $DEVICE == 'r2s' || $DEVICE == 'r2c' || $DEVICE == 'r1p' || $DEVICE == 'r1p-lts' ]]; then
-  sed -i 's/5.10/5.4/g' target/linux/rockchip/Makefile
-  line_number_CONFIG_CRYPTO_LIB_BLAKE2S=$[`grep -n 'CONFIG_CRYPTO_LIB_BLAKE2S' package/kernel/linux/modules/crypto.mk | cut -d: -f 1`+1]
-  sed -i $line_number_CONFIG_CRYPTO_LIB_BLAKE2S' s/HIDDEN:=1/DEPENDS:=@(LINUX_5_4||LINUX_5_10)/' package/kernel/linux/modules/crypto.mk
-  sed -i 's/libblake2s.ko@lt5.9/libblake2s.ko/;s/libblake2s-generic.ko@lt5.9/libblake2s-generic.ko/' package/kernel/linux/modules/crypto.mk
-fi
+case $DEVICE in
+  r2s|r2c|r1p|r1p-lts)
+    sed -i 's/5.10/5.4/g' target/linux/rockchip/Makefile
+    line_number_CONFIG_CRYPTO_LIB_BLAKE2S=$[`grep -n 'CONFIG_CRYPTO_LIB_BLAKE2S' package/kernel/linux/modules/crypto.mk | cut -d: -f 1`+1]
+    sed -i $line_number_CONFIG_CRYPTO_LIB_BLAKE2S' s/HIDDEN:=1/DEPENDS:=@(LINUX_5_4||LINUX_5_10)/' package/kernel/linux/modules/crypto.mk
+    sed -i 's/libblake2s.ko@lt5.9/libblake2s.ko/;s/libblake2s-generic.ko@lt5.9/libblake2s-generic.ko/' package/kernel/linux/modules/crypto.mk
+  ;;
+esac
 
 # ...
-git revert d15af9ff7c534853695a52bb94f07beb4ffec02a
-sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/' target/linux/rockchip/image/armv8.mk target/linux/sunxi/image/cortexa53.mk target/linux/sunxi/image/cortexa7.mk
+if [[ $DEVICE != 'r5s' ]]; then
+  sed -i 's/kmod-usb-net-rtl8152/kmod-usb-net-rtl8152-vendor/' target/linux/rockchip/image/armv8.mk target/linux/sunxi/image/cortexa53.mk target/linux/sunxi/image/cortexa7.mk
+fi
